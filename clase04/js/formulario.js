2e3efb435d2ecab2ce62e59cb209e9ec4fd54512
marcos pinardi <exegeses@gmail.com>
2020-04-25 18:46:56 -0300

    validacion de formulario con estetica bien piola

--- clase04/js/formulario.js
+++ clase04/js/formulario.js
@@ -1,4 +1,33 @@
 // ubicar elemento dentro del DOM
 
+    var campoNombre = document.querySelector('input[name="nombre"]');
+    var errorNombre = document.querySelector('#errorNombre');
+
+    var campoEmail = document.querySelector('input[name="email"]');
+    var errorEmail = document.querySelector('#errorEmail');
+
+
 // generar función
 
+    function validar()
+    {
+        //vaciar campos de error
+        errorNombre.innerHTML = '';
+        errorEmail.innerHTML = '';
+
+        //chequear que haya algo en campo nombre
+        if( campoNombre.value == '' | campoNombre.value == null ){
+
+            errorNombre.innerHTML = '<img src="imagenes/error2.png"> Complete el campo Nombre';
+            return false;
+        }
+
+        //chequear que haya algo en campo email
+        if ( campoEmail.value == '' | campoEmail.value == null ){
+            errorEmail.innerHTML = '<img src="imagenes/error2.png"> Complete el campo Email';
+            return false;
+        }
+
+        return true;
+
+    }
